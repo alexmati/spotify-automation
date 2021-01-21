e6abfc050d63d370f119808abb2831ce533eeb27
Alex Mati <alexmati100@gmail.com>
2021-01-21 17:43:10 +0000

    implemented mux routers

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,17 +1,25 @@
 package server
 
 import (
-	"io"
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func Run() {
-	con := func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Connected\n")
-	}
-
-	http.HandleFunc("/", con)
-
+	r := mux.NewRouter()
+	r.HandleFunc("/connected", connectedHandler).Methods("GET")
+	r.HandleFunc("/disconnected", disconnectedHandler).Methods("GET")
+	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func connectedHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Connected!")
+}
+
+func disconnectedHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Disconnected!")
 }
